b0161506d3c2842546d40188f1b7208986e7ddae
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2022-10-01 14:38:26 -0300

    File&FileInfo

---
+++ File_And_FileInfo/Program.cs
@@ -0,0 +1,39 @@
+﻿using static System.Console;
+
+WriteLine("What's the file name?");
+var nameFile = ReadLine();
+
+nameFile = SanitizeNameFile(nameFile);
+
+var path = Path.Combine(Environment.CurrentDirectory, $"{nameFile}.txt");
+
+try
+{
+  CreateFile(path);
+}
+catch
+{
+
+  WriteLine("Couldn't create file, please try again");
+}
+
+WriteLine("Press enter to continue...");
+ReadLine();
+
+static string SanitizeNameFile(string nameFile)
+{
+  foreach (var @char in Path.GetInvalidFileNameChars())
+  {
+    nameFile = nameFile.Replace(@char, '-');
+  }
+
+  return nameFile;
+}
+
+static void CreateFile(string path)
+{
+  using var sw = File.CreateText(path);
+  sw.WriteLine("Line 1");
+  sw.WriteLine("Line 2");
+  sw.WriteLine("Line 3");
+}
