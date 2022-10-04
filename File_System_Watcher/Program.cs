e0ff8069fefd0fbed03802b50b83da9d3ae1f83a
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2022-10-03 21:03:44 -0300

    FileSystemWatcher into a studies

---
+++ File_System_Watcher/Program.cs
@@ -0,0 +1,44 @@
+﻿using static System.Console;
+
+var path = @"C:\temp\globe";
+using var fsw = new FileSystemWatcher(path);
+fsw.Created += OnCreated;
+fsw.Renamed += OnRenamed;
+fsw.Deleted += OnDeleted;
+
+fsw.EnableRaisingEvents = true;
+fsw.IncludeSubdirectories = true;
+
+WriteLine("Listening path...");
+ReadLine();
+
+void OnCreated(object sender, FileSystemEventArgs e)
+{
+  if (e.Name.Contains(".txt"))
+  {
+    WriteLine($"Creates a new file: {e.Name}");
+    return;
+  }
+  WriteLine($"Creates a new directory: {e.Name}");
+}
+
+void OnRenamed(object sender, RenamedEventArgs e)
+{
+  if (e.OldName.Contains(".txt") && e.Name.Contains(".txt"))
+  {
+    WriteLine($"Rename file {e.OldName} to {e.Name}");
+    return;
+  }
+
+  WriteLine($"Rename directory {e.OldName} to {e.Name}");
+}
+
+void OnDeleted(object sender, FileSystemEventArgs e)
+{
+  if (e.Name.Contains(".txt"))
+  {
+    WriteLine($"Deletes a file: {e.Name}");
+    return;
+  }
+  WriteLine($"Deletes a directory: {e.Name}");
+}
